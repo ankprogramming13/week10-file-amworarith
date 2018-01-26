9fcdf7c5567e2c088e9bd0283df0509deb15bb28
Mr. Worarith Ketma <33056474+amworarith@users.noreply.github.com>
2018-01-26 13:47:08 +0700

    Update lab10_2.cpp

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -1,4 +1,23 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
 int main() {
         /* code */
+        string line;
+        ifstream myfile ("example.txt");
+        if (myfile.is_open())
+        {
+            while ( getline(myfile,line) )
+            {
+                cout << line << '\n';
+            }
+            myfile.close();
+        }
+        else cout << "Unable to open file";
         return 0;
 }
+ประกาศออบเจ็ต string ขึ้นมา เพื่อเก็บค่า line
+บรรทัดต่อมาประกาศตัวแปร myfile 
+ทำการเขียนประแกรมเพื่อวนลูบ
